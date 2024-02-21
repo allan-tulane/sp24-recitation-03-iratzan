55860093a049c980d935ad42bdc6afdaa20a8833
iratzan <126986318+iratzan@users.noreply.github.com>
2024-02-21 20:36:25 +0000

    Final

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ class BinaryNumber:
     def __init__(self, n):
         self.decimal_val = n               
         self.binary_vec = list('{0:b}'.format(n)) 
-        
+
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
-    
+
 
 ## Implement multiplication functions here. Note that you will have to
 ## ensure that x, y are appropriately sized binary vectors for a
@@ -30,7 +30,7 @@ def split_number(vec):
 def bit_shift(number, n):
     # append n 0s to this number's binary string
     return binary2int(number.binary_vec + ['0'] * n)
-    
+
 def pad(x,y):
     # pad with leading 0 if x/y have different number of bits
     # e.g., [1,0] vs [1]
@@ -50,19 +50,33 @@ def quadratic_multiply(x, y):
 
 def _quadratic_multiply(x, y):
     ### TODO
-    pass
-    ###
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    padded = pad(xvec, yvec)
+    xvec = padded[0]
+    yvec = padded[1]
+    if (binary2int(xvec).decimal_val <= 1) and (binary2int(yvec).decimal_val <= 1):
+        return BinaryNumber(binary2int(xvec).decimal_val * binary2int(yvec).decimal_val)
+    else:
+        x_split = split_number(xvec)
+        y_split = split_number(yvec)
+        x_left = x_split[0]
+        x_right = x_split[1]
+        y_left = y_split[0]
+        y_right = y_split[1]
+        a = bit_shift((_quadratic_multiply(x_left, y_left)), len(xvec))
+        b = bit_shift(_quadratic_multiply(x_left, y_right), len(xvec) // 2)
+        c = bit_shift(_quadratic_multiply(x_right, y_left), len(xvec) // 2)
+        d = bit_shift(_quadratic_multiply(x_right, y_right), 0)
+        return BinaryNumber(a.decimal_val + b.decimal_val + c.decimal_val + d.decimal_val)
+    ###  
 
-
-    
-    
-def test_quadratic_multiply(x, y, f):
+def test_quadratic_multiply():
     start = time.time()
     # multiply two numbers x, y using function f
-    
+    print(_quadratic_multiply(BinaryNumber(20), BinaryNumber(8)))
+    print(_quadratic_multiply(BinaryNumber(7), BinaryNumber(5)))
+
     return (time.time() - start)*1000
 
-
-    
-    
-
+print(test_quadratic_multiply())
--- test_main.py
+++ test_main.py
@@ -5,3 +5,6 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
+
+
+print(test_multiply())
